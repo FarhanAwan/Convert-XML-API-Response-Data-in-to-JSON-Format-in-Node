8400888798a9247400168533ffac3e1c96c6a08f
farhan awan <farhankhanawan@gmail.com>
2022-10-18 22:39:44 +0500

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+// import { response } from 'express';
+import fetch from 'node-fetch';
+import {parseString} from 'xml2js';
+// const {parseString} = require('xml2js');
+// const fetch = require("node-fetch");
+fetch('http://restapi.adequateshop.com/api/Traveler?page=1', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'text/xml'
+  }
+
+}).then(response=>{
+  return response.text();
+}).then(responseText => {
+  var xmlData = responseText;
+  parseString(xmlData, function(err, result){
+    var data = JSON.stringify(result)
+    console.log(data);
+  })
+
+}).catch(error => console.log(error))
+;
